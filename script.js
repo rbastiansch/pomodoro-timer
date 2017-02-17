e96d7550956ad3b944bb4ff69881343680deff01
Rafael Bastiansch <rafaelbastiansch@gmail.com>
2017-02-16 22:07:06 -0200

    add current task, change to arrow functions

--- script.js
+++ script.js
@@ -1,58 +1,55 @@
 let countdown;
 let verifyTimer = 1;
+let verifyTask = 1;
 const timerDisplay = document.querySelector('.display-time-left');
 const instructionDisplay = document.querySelector('.display-instruction');
+const currentTaskDisplay = document.querySelector('.display-current-task');
 const buttons = document.querySelector('.timer-button');
+const audio = document.querySelector('audio');
+const seconds = document.querySelector('.timer-button').dataset.time;
 
-function playAudio(){
-	const audio = document.querySelector(`audio`);
-	audio.play();
-}
+const playAudio = () => { audio.play(); }
 
-function verifyPomodoro(seconds){
+const verifyPomodoro = () => {
 	if(verifyTimer == 7){
 		playAudio();
 		timer(1800);
 		instructionDisplay.textContent = "Time to a BIG rest";
-
 	}else if(verifyTimer%2 == 0){
-			
+			verifyTask++;			
 			playAudio();
 			timer(1500);
 			instructionDisplay.textContent = "Time to work";
+			currentTaskDisplay.textContent = `Current pomo:  ${verifyTask}`;
 		}else{			
 			playAudio();
 			timer(300);
-			instructionDisplay.textContent = "Time to rest";
+			instructionDisplay.textContent = "Time to rest";			
 		}
 }
 
-function timer(seconds){
-
+const timer = seconds => {
 	clearInterval(countdown);
-
 	const now = Date.now();
 	const then = now + seconds * 1000;
-	displayTimeLeft(seconds);
 
 	countdown = setInterval(() => {
 		const secondsLeft = Math.round((then - Date.now()) / 1000);
-		
-		displayTimeLeft(secondsLeft);
+
 		if(secondsLeft <= 0 && verifyTimer < 8){
 			clearInterval(countdown);
-			verifyPomodoro(secondsLeft);
+			verifyPomodoro();
 			verifyTimer++;
 			
 		}else if(secondsLeft <= 0){
 			clearInterval(countdown);
 			return;
 		}
-		
+		displayTimeLeft(secondsLeft);		
 	}, 1000);
 }
 
-function displayTimeLeft(seconds){
+const displayTimeLeft = seconds => {
 	const minutes = Math.floor(seconds / 60);
 	const remainderSeconds = seconds % 60;
 	const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
@@ -60,11 +57,12 @@ function displayTimeLeft(seconds){
 	document.title = display;
 }
 
-function startTimer(){
-	const seconds = parseInt(this.dataset.time);
+const startTimer = () => {	
 	verifyTimer = 1;
+	displayTimeLeft(seconds);
 	timer(seconds);
 	instructionDisplay.textContent = "Time to work";
+	currentTaskDisplay.textContent = `Current Pomo:  ${verifyTask}`;
 }
 
 buttons.addEventListener('click', startTimer);
